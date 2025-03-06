ddd1f04b15b3ea71162432bdcbc218fd50760ea2
erasmochale253 <136523134+erasmochale253@users.noreply.github.com>
2025-03-06 19:45:50 +0200

    close and LinkedIn image not found fixed

--- js/footer.js
+++ js/footer.js
@@ -12,7 +12,7 @@ function footerTemplate(){
                 <a href="#"><img src="./img/Social Media Icon/Twitter.svg" alt="Twitter"></a>
                 <a href="#"><img src="./img/Social Media Icon/Pinterest.svg" alt="Pinterest"></a>
                 <a href="#"><img src="./img/Social Media Icon/Behance.svg" alt="Behance"></a>
-                <a href="#"><img src="./img/Social Media Icon/Linkedin.svg" alt="linkedin"></a>
+                <a href="#"><img src="./img/Social Media Icon/LinkedIn.svg" alt="linkedin"></a>
                 <a href="#"><img src="./img/Social Media Icon/Dribble.svg" alt="linkedin"></a>
             </div>
             <p>Christopher, &copy; ${dateYear} All Rights Reserved.</p>`
